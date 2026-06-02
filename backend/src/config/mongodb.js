8951a1b88d573c843c7088621fb9b303e3a6ae78
Idan Stark <idanstark42@gmail.com>
2026-06-03 01:29:48 +0300

    added mongodb name

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export default async function connectMongo() {
-  await mongoose.connect(process.env.MONGODB_URI)
+  await mongoose.connect(process.env.MONGODB_URI, { dbName: process.env.MONGODB_NAME })
 
   console.log("MongoDB connected")
 }
