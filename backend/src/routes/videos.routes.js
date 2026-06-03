465cc93f262b3dbffd5c4d8fa34a1837021407b7
Idan Stark <idanstark42@gmail.com>
2026-06-03 21:44:09 +0300

    video upload bugfix?

--- backend/src/routes/videos.routes.js
+++ backend/src/routes/videos.routes.js
@@ -8,8 +8,8 @@ import { ROLES } from "../constants/roles.js"
 const router = Router()
 
 router.get("/", auth, videosController.getAll)
-router.get("/:id", auth, videosController.getById)
 router.get("/upload-signature", auth, requireRole(ROLES.ADMIN), videosController.getSignature);
+router.get("/:id", auth, videosController.getById)
 router.post("/", auth, requireRole(ROLES.ADMIN), videosController.create);
 router.patch("/:id", auth, requireRole(ROLES.ADMIN), videosController.update)
 router.delete("/:id", auth, requireRole(ROLES.ADMIN), videosController.delete)
